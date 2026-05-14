babf8aeb07f8a93598a1bded70b3e1dd0c6033d6
Sandy Wake <sandy.wake@mckennaconsultants.com>
2026-05-14 14:28:14 +0100

    Finished Gatsby Tutorial Part 6

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
-import Layout from '../components/layout'
-import Seo from '../components/seo'
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/layout'
+import Seo from '../../components/seo'
 
 const BlogPage = ({ data }) => {
   return (
@@ -10,7 +10,12 @@ const BlogPage = ({ data }) => {
       {
         data.allMdx.nodes.map((node) => (
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
+            <h2>
+              <Link to={`/blog/${node.frontmatter.slug}`}>
+                {node.frontmatter.title}
+                {console.log(node)}
+              </Link>
+            </h2>
             <p>Posted: {node.frontmatter.date}</p>
             <p>{node.excerpt}</p>
           </article>
@@ -23,15 +28,15 @@ const BlogPage = ({ data }) => {
 
 export const query = graphql`
   query {
-    allMdx {
+    allMdx(sort: { frontmatter: { date: DESC }}) {
       nodes {
-        frontmatter{
-            date(formatString: "MMMM D, YYYY")
-            title
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+          slug
         }
         id
-        excerpt
-    }
+      }
     }
   }
 `
